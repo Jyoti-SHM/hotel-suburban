78baf72dfe202bdb211114d1f002754cb6758e39
Jyoti-SHM <jyotihospitalityminds@gmail.com>
2025-05-10 12:19:04 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    $to = "satkkarhotel@gmail.com";
+    $to = "hotelsuburban01@gmail.com";
     $subject = "New Contact Form Submission";
     $headers = "From: $email";
 
